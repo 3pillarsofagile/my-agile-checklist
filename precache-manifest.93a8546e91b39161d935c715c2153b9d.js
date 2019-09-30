52a8d257a564088445333def3f7d76bfc6838e9f
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-09-30 14:40:40 +0700

    Updates

--- precache-manifest.93a8546e91b39161d935c715c2153b9d.js
+++ precache-manifest.93a8546e91b39161d935c715c2153b9d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa396c109cbdb7fca10671bfb9a7cf3c",
+    "revision": "5809778b1e3d8e19c2d8b730b2cc4c1f",
     "url": "/my-agile-checklist/index.html"
   },
   {
-    "revision": "643f539a5bfb25f449ab",
+    "revision": "84f09b8e1975a58cb5e4",
     "url": "/my-agile-checklist/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "dd3f54ba3b185c9e0603",
+    "revision": "0abaae928655e0fc6e8e",
     "url": "/my-agile-checklist/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "643f539a5bfb25f449ab",
-    "url": "/my-agile-checklist/static/js/2.63835d65.chunk.js"
+    "revision": "84f09b8e1975a58cb5e4",
+    "url": "/my-agile-checklist/static/js/2.12846819.chunk.js"
   },
   {
-    "revision": "dd3f54ba3b185c9e0603",
-    "url": "/my-agile-checklist/static/js/main.a3950972.chunk.js"
+    "revision": "0abaae928655e0fc6e8e",
+    "url": "/my-agile-checklist/static/js/main.892fe319.chunk.js"
   },
   {
     "revision": "66357ea75b27c638494c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-agile-checklist/precache-manifest.51c8d53e64d21a28a8dd42f15c6a8566.js"
+  "/my-agile-checklist/precache-manifest.93a8546e91b39161d935c715c2153b9d.js"
 );
 
 self.addEventListener('message', (event) => {
