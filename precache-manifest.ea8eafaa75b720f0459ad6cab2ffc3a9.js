1f2885a527a93fd8db2a74539bb03bd2ffa48a02
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-10-02 15:52:37 +0700

    Updates

---
+++ precache-manifest.ea8eafaa75b720f0459ad6cab2ffc3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0188a4020147a58275032fe70163eb93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8a552fcb66d1b98dc87",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7861d4004ac935e69853",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d8a552fcb66d1b98dc87",
+    "url": "/static/js/2.fb182941.chunk.js"
+  },
+  {
+    "revision": "7861d4004ac935e69853",
+    "url": "/static/js/main.e20de307.chunk.js"
+  },
+  {
+    "revision": "280ca6853eb989e80a3a",
+    "url": "/static/js/runtime-main.dce75136.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-agile-checklist/precache-manifest.011b10365e7317f34b8fac570b2b9b11.js"
+  "/precache-manifest.ea8eafaa75b720f0459ad6cab2ffc3a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-agile-checklist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
